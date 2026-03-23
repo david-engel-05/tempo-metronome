44a7fa9e3fd493bad841c6159952d82a1e713ac0
Yanis Dängeli <info@yan-is.ch>
2026-03-23 14:18:10 +0100

    leere ios app

---
+++ tempo-metronome/tempo-metronome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tempo-metronome
+//
+//  Created by Yanis Dängeli on 23.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tempo-metronome/tempo-metronome/tempo_metronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempo_metronomeApp.swift
+//  tempo-metronome
+//
+//  Created by Yanis Dängeli on 23.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct tempo_metronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
